26c0d9a3994a3eb3cc2e9cc4e412cee70e7300ca
Hoàng Thị Huyền <huyenht@VNIMATION.COM>
2023-11-10 09:32:26 +0700

    add order management

---
+++ resources/js/Pages/Test/OrderHome.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+
+
+
+</script>
+<template>
+
+
+              <Link
+              
+              href=""
+              class="min-[320px]:my-2 text-sm px-3 py-2 border rounded-lg mx-1 bg-gray-100 hover:bg-white text-gray-500"
+              :class="{ 'bg-white text-blue-500': '' }"
+            >
+              Đơn Chờ
+              <!-- <span class="text-gray-400 ml-1"></span> -->
+            </Link> 
+            <Link
+              
+              href=""
+              class="min-[320px]:my-2 text-sm px-3 py-2 border rounded-lg mx-1 bg-gray-100 hover:bg-white text-gray-500"
+              :class="{ 'bg-white text-blue-500': '' }"
+            >
+              Đơn Hoàn
+              <!-- <span class="text-gray-400 ml-1"></span> -->
+            </Link> 
+              <Link
+              
+                href=""
+                class="min-[320px]:my-2 text-sm px-3 py-2 border rounded-lg mx-1 bg-gray-100 hover:bg-white text-gray-500"
+                :class="{ 'bg-white text-blue-500': '' }"
+              >
+                Đơn hủy
+                <!-- <span class="text-gray-400 ml-1"></span> -->
+              </Link>
+          
+    
+   
+
+</template>
+
