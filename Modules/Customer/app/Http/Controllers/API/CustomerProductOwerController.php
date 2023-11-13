647c010d9d7af98fd3b04391cb3225c7e671c683
Nguyen Nga <ngant1096@gmail.com>
2023-11-13 16:54:36 +0700

    edit

--- Modules/Customer/app/Http/Controllers/API/CustomerProductOwerController.php
+++ Modules/Customer/app/Http/Controllers/API/CustomerProductOwerController.php
@@ -17,9 +17,7 @@ class CustomerProductOwerController extends Base2Controller
    //all product of user
    public function getProductService()
    {
-        $token = PersonalAccessToken::findToken(request()->bearerToken());
-        if($token){
-            $customer = $token->tokenable;
+            $customer = Auth::user();
             // $product_owner = $customer->product_service_owners;
             $product_owner = ProductServiceOwner::with('product.images')->where('user_id',$customer->id)->get();
             $product_not_owner = ProductServiceOwner::with('product.images')->where('user_id','!=',$customer->id)->get();
@@ -30,22 +28,15 @@ class CustomerProductOwerController extends Base2Controller
                 'not_owner' => $product_not_owner
             ];
             return $this->sendResponse($response, 'Get apartmentDetail successfully');
-        }
-
-        return response()->json('not found token', 200);
    }
 
    public function getOneProductActivity($id)
    {
-    $token = PersonalAccessToken::findToken(request()->bearerToken());
-    if($token){
-        $customer = $token->tokenable;
+        $customer = Auth::user();
         $product_owner = ProductServiceOwner::with('product.images','trees','contract','history_use_service')->where('user_id',$customer->id)->find($id);
         $response = [
             'product_detail' =>$product_owner
         ];
         return $this->sendResponse($response, 'Get apartmentDetail successfully');
-    }
-    return response()->json('not found token', 200);
    }
 }
