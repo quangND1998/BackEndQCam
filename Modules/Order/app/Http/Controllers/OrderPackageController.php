eedc400a1bb934a375fc1bf0092f6aca32ba149f
ngant1096@gmail.com <ngant1096@gmail.com>
2023-12-06 23:54:05 +0700

    edit

--- Modules/Order/app/Http/Controllers/OrderPackageController.php
+++ Modules/Order/app/Http/Controllers/OrderPackageController.php
@@ -107,7 +107,8 @@ class OrderPackageController extends Controller
     }
     public function orderPackage(Request $request){
         $user = Auth::user();
-        $sales = User::role('saler')->get();
+        $sales = User::with('team')->has('team', '!=', 0)->role('saler')->get();
+        return $sales;
         $leaders = User::role('leader-sale')->get();
         $telesale = User::role('telesale')->get();
         $ctv = User::role('ctv')->get();
