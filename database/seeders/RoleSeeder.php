15394821b02e9e47d5463e7d552b04db169cb74d
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2023-12-06 10:39:23 +0700

    serve

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -30,18 +30,20 @@ class RoleSeeder extends Seeder
             'update-product',
             'delete-product',
             // order permission
-            'order-pending', 'order-packing', 'order-shipping', 'order-completed', 'order-refund', 'order-decline'
+            'order-pending', 'order-packing', 'order-shipping', 'order-completed', 'order-refund', 'order-decline','order','complaint','change-state-order',
+            'contract-pending',   'contract-create',   'contract-cancle',   'contract-complete',   'create-contract-complete',   'add-new-package',   'view-news',
+            'create-news', 'update-news', 'delete-news'
         ]);
-        $role->givePermissionTo(['super-admin', 'update-user', 'delete-user', 'create-user', 'view-user', 'view-land',  'create-land',  'update-land', 'delete-land']);
+       
 
-        $role2 = Role::create(['name' => 'customer']);
-        $role2->givePermissionTo(['login-app', 'order', 'review', 'complaint']);
+        $customer = Role::create(['name' => 'customer']);
+        $customer->givePermissionTo(['login-app', 'order', 'review', 'complaint']);
 
-        $role3 = Role::create(['name' => 'shipper']);
-        $role3->givePermissionTo(['shipper']);
+        $shipper = Role::create(['name' => 'shipper']);
+        $shipper->givePermissionTo(['shipper']);
 
-        $role3 = Role::create(['name' => 'leader-sale']);
-        $role3->givePermissionTo([
+        $leader_sale = Role::create(['name' => 'leader-sale']);
+        $leader_sale->givePermissionTo([
             'update-user',
             'delete-user',
             'create-user',
@@ -49,9 +51,47 @@ class RoleSeeder extends Seeder
             'order-pending', 'order-packing', 'order-shipping', 'order-completed', 'order-refund', 'order-decline'
         ]);
 
-        $role3 = Role::create(['name' => 'saler']);
-        $role3->givePermissionTo([
-            'order-pending', 'order-packing', 'order-shipping', 'order-completed', 'order-refund', 'order-decline'
+        $saler = Role::create(['name' => 'saler']);
+        $saler->givePermissionTo(['saler',
+            'order-pending', 'order-packing', 'order-shipping', 'order-completed', 'order-refund', 'order-decline','add-new-package',
+            'contract-pending',    'contract-cancle',   'contract-complete', 
         ]);
+
+
+        $Ketoan = Role::create(['name' => 'Kế toán']);
+        $Ketoan->givePermissionTo(['saler',
+            'order-pending', 'order-packing', 'order-shipping', 'order-completed', 'order-refund', 'order-decline','create-contract-complete',   'add-new-package', 
+            'contract-pending','contract-create',  'contract-cancle',   'contract-complete', 
+        ]);
+
+
+
+        $shopmanager = Role::create(['name' => 'shopmanager']);
+        $shopmanager->givePermissionTo(['saler',
+            'view-product', 'create-product', 'update-product',  'add-new-package'
+        ]);
+
+        $cskh = Role::create(['name' => 'cskh']);
+        $cskh->givePermissionTo(['saler','document-customer', 'package-customer', 'info-customer',  'viewer-customer']);
+
+        $event = Role::create(['name' => 'event']);
+        $event->givePermissionTo(['order','complaint','change-state-order','order-pending', 'order-packing', 'order-shipping', 'order-completed', 'order-refund', 'order-decline','saler',
+        'contract-pending','contract-create',  'contract-cancle',   'contract-complete']);
+
+
+        $quanlytintuc = Role::create(['name' => 'quản lý tin tức']);
+        $quanlytintuc->givePermissionTo([ 'view-news',
+        'create-news', 'update-news', 'delete-news']);
+
+        $thongtinchung = Role::create(['name' => 'thông tin chung']);
+        $thongtinchung->givePermissionTo([ 'view-setting',
+        'view-notification', 'view-setting-privacy', 'view-setting-contact']);
+
+
+        $ctv = Role::create(['name' => 'ctv']);
+        $ctv->givePermissionTo([ 'view-news']);
+
+        $telesale = Role::create(['name' => 'telesale']);
+        $telesale->givePermissionTo([ 'view-news']);
     }
 }
--- resources/js/Components/PillTagPlain.vue
+++ resources/js/Components/PillTagPlain.vue
@@ -15,7 +15,7 @@ defineProps({
 </script>
 
 <template>
-  <div class="inline-flex items-center capitalize leading-none" :class="[small ? 'text-xs' : 'text-sm']">
+  <div class="inline-flex items-center leading-none" :class="[small ? 'text-xs' : 'text-sm']">
     <BaseIcon v-if="icon" :path="icon" h="h-4" w="w-4" :class="small ? 'mr-1' : 'mr-2'" :size="small ? 14 : null" />
     <span>{{ label }}</span>
   </div>
