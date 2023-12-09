57f21c38268d1f460133603fa5fd37fa50e767ed
Nguyen Nga <ngant1096@gmail.com>
2023-12-09 09:39:00 +0700

    edit

--- resources/js/Pages/Modules/Order/Package/editOrderPackage.vue
+++ resources/js/Pages/Modules/Order/Package/editOrderPackage.vue
@@ -755,7 +755,7 @@ const date = ref(new Date());
                                     Số tiền</label>
                                 <!-- <MazInputPrice v-model="form.price_percent" currency="VND" locale="vi-VN" :min="0" :disabled="order?.history_payment.length > 0"
                                     :max="maxPrice" @formatted="formattedPrice = $event" /> -->
-                                    <InputNumber v-model="form.price_percent" :min="0"  :max="maxPrice"
+                                    <InputNumber v-model="form.price_percent" :min="0"  :max="maxPrice" :disabled="order?.history_payment.length > 0"
                                     inputClass="bg-gray-50 border border-gray-300 text-gray-900 text-sm border_round focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                             </div>
                             <div class="w-1/2">
