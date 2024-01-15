ded147dddf89b0879ad156ff33ca8c87df1b8ada
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2024-01-15 15:38:45 +0700

    quangnd

--- app/Http/Controllers/API/NotificationController.php
+++ app/Http/Controllers/API/NotificationController.php
@@ -8,25 +8,29 @@ use Illuminate\Support\Facades\Auth;
 
 class NotificationController extends Controller
 {
-    public function notifications(){
+    public function notifications()
+    {
         $user = Auth::user();
         $notifications = $user->notifications;
         return response()->json($notifications, 200);
     }
 
-    public function getUnreadNotifications(){
+    public function getUnreadNotifications()
+    {
         $user = Auth::user();
-        $notifications = $user->unreadNotifications ;
+        $notifications = $user->unreadNotifications;
         return response()->json($notifications, 200);
     }
 
-    public function readNotifications(){
+    public function readNotifications()
+    {
         $user = Auth::user();
         $user->unreadNotifications->markAsRead();
         return response()->json($user->notifications, 200);
     }
 
-    public function deleteNotifications(){
+    public function deleteNotifications()
+    {
         $user = Auth::user();
         $user->notifications()->delete();
         return response()->json($user->notifications, 200);
