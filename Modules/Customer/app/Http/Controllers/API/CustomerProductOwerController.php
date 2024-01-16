335f7b4acb408405d803932d25b365bdba531b1b
Nguyen Nga <ngant1096@gmail.com>
2024-01-16 17:36:52 +0700

    edit

--- Modules/Customer/app/Http/Controllers/API/CustomerProductOwerController.php
+++ Modules/Customer/app/Http/Controllers/API/CustomerProductOwerController.php
@@ -25,7 +25,7 @@ class CustomerProductOwerController extends Base2Controller
             if($customer){
                 // $product_owner = $customer->product_service_owners;
                 // update tree care
-                $product_owner = ProductServiceOwner::with('product.images','history_gift','tree.images','tree.history_care')->where('user_id',$customer->id)->get();
+                $product_owner = ProductServiceOwner::with('product.images','history_gift','tree.images','tree.history_care.activityCare')->where('user_id',$customer->id)->get();
                 $product_not_owner = ProductService::with('images')->whereDoesntHave('productServiceOwner')->where('status',1)->get();
 
                 $response = [
