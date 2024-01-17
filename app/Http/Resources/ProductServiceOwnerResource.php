4bb77828e577f2a6c23dc258386e697ff998f60b
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2024-01-17 13:24:53 +0700

    quangnd

--- app/Http/Resources/ProductServiceOwnerResource.php
+++ app/Http/Resources/ProductServiceOwnerResource.php
@@ -17,23 +17,6 @@ class ProductServiceOwnerResource extends JsonResource
         return array_merge(parent::toArray($request), [
             "tree" => new TreeResource($this->tree)
         ]);
-        // return [
-        //     "id" => $this->id,
-        //     "user_id" => $this->user_id,
-        //     "product_service_id" => $this->product_service_id,
-        //     "time_approve" => $this->time_approve,
-        //     "description" => $this->description,
-        //     "number_deliveries_current" => $this->number_deliveries_current,
-        //     "state" => $this->state,
-        //     "visited_time" => $this->visited_time,
-        //     "quantity_delivered" => $this->quantity_delivered,
-        //     "created_at" => $this->created_at,
-        //     "updated_at" => $this->updated_at,
-        //     "time_end" => $this->time_end,
-        //     "order_id" => $this->order_id,
-        //     "price" => $this->price,
-        //     "tree" => new TreeResource($this->tree),
-
-        // ];
+       
     }
 }
