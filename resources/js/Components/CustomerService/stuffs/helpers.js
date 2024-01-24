d1bd544937ace86c504adef72ca2cde140b509ca
HuyDQ <dqhuy78@gmail.com>
2024-01-24 11:53:54 +0700

    Update helpers.js

--- resources/js/Components/CustomerService/stuffs/helpers.js
+++ resources/js/Components/CustomerService/stuffs/helpers.js
@@ -10,3 +10,26 @@ export const getCycleYear = (lifeTime, timeApprove) => {
 
   return `(${dateMilestones.findIndex(dateMilestone => dateMilestone.diff(new Date(), 'seconds') > 0) + 1})`;
 }
+
+export const generatePageNumbers(currentPage, totalPages) {
+  let startPage, endPage;
+  if (totalPages <= 5) {
+    startPage = 1;
+    endPage = totalPages;
+  } else {
+    if (currentPage <= 3) {
+      startPage = 1;
+      endPage = 5;
+    } else if (currentPage + 2 >= totalPages) {
+      startPage = totalPages - 4;
+      endPage = totalPages;
+    } else {
+      startPage = currentPage - 2;
+      endPage = currentPage + 2;
+    }
+  }
+
+  let pageNumbers = Array.from({length: (endPage - startPage + 1)}, (_, i) => startPage + i);
+
+  return pageNumbers;
+}
