50561b68a4f4dd57f42f488ac533139428afdf96
HuyDQ <dqhuy78@gmail.com>
2024-01-24 13:52:01 +0700

    Update Table.vue

--- resources/js/Components/CustomerService/RemindTable/Table.vue
+++ resources/js/Components/CustomerService/RemindTable/Table.vue
@@ -1,15 +1,30 @@
 <script setup>
-  import { inject, computed } from 'vue';
+  import { inject, computed, ref } from 'vue';
 
-  import Row from '@/Components/CustomerService/RemindTable/Row.vue';
+  import useQuery from '@/Components/CustomerService/composables/useQuery';
   import { generatePageNumbers } from '@/Components/CustomerService/stuffs/helpers';
+  import Row from '@/Components/CustomerService/RemindTable/Row.vue';
+  import Spinner from '@/Components/CustomerService/SpinnerIcon.vue';
 
   const { remindData } = inject('REMIND');
+  const reminds = ref(props.remindData.data);
 
-  const reminds = computed(() => remindData.data);
-  const currentPage = computed(() => remindData.current_page || 0);
+  const { isLoading, executeQuery } = useQuery(
+    CUSTOMER_SERVICE_API_MAKER.GET_REMIND(),
+    undefined,
+    (response) => {
+      reminds.value = response.data
+    }
+  );
+  const itemPerPage = ref(5);
+  const currentPage = ref(remindData.current_page || 0);
   const lastPage = computed(() => remindData.last_page || 0);
   const pageNumbers = computed(() => generatePageNumbers(currentPage.value, lastPage.value));
+  const onChangePage = (pageNo) => {
+    if (pageNo === currentPage.value) return;
+    currentPage.value = pageNo;
+    executeQuery({ pageNo, itemPerPage });
+  }
 </script>
 
 <template>
@@ -24,47 +39,48 @@
     <div class="col-span-2">Lịch hẹn gọi lại</div>
     <div class="col-span-2">Chăm sóc</div>
   </div>
-  <Row v-for="(remind, index) in reminds" :key="remind.id" :index="index" :remind="remind" />
-  <template v-if="reminds.length < 5">
-    <div v-for="n in (5 - reminds.length)" :key="n"
-      class="grid grid-cols-[repeat(28,_minmax(0,_1fr))] divide-x text-sm bg-white border-x border-b !border-gray-400 divide-gray-400 items-center  text-center font-semibold leading-10">
-      <div>&#8203;</div>
-      <div class="col-span-3">&#8203;</div>
-      <div class="col-span-2">&#8203;</div>
-      <div class="col-span-4">&#8203;</div>
-      <div class="col-span-2">&#8203;</div>
-      <div class="text-left pl-2 col-span-12">&#8203;</div>
-      <div class="col-span-2">&#8203;</div>
-      <div class="col-span-2 grid items-center">&#8203;</div>
-    </div>
-  </template>
-  <div class="flex items-center justify-between mt-3">
-    <div class="flex items-center gap-4">
-      <p>Hiển thị</p>
-      <select
-        class="border !border-gray-400 rounded-sm px-2 py-1 !w-20 focus:outline-none focus:!border-gray-400 focus:ring-0">
-        <option>5</option>
-        <option>10</option>
-        <option>20</option>
-      </select>
-    </div>
-    <div class="flex items-center gap-2">
-      <p>Trang:</p>
-      <div class="flex items-center gap-3">
-        <ul class="flex items-center gap-1">
-          <li>
-              <span aria-current="page" class="page-number active">
-                1
-              </span>
-          </li>
-          <li>
-              <span class="page-number">
-                2
-              </span>
-          </li>
-      </ul>
+  <div v-if="isLoading" class="w-full h-[200px] flex items-center justify-center">
+    <SpinnerIcon class="!m-0 text-2xl text-orange-600" />
+  </div>
+  <template v-else>
+    <Row v-for="(remind, index) in reminds" :key="remind.id" :index="index" :remind="remind" />
+    <template v-if="reminds.length < 5">
+      <div v-for="n in (5 - reminds.length)" :key="n"
+        class="grid grid-cols-[repeat(28,_minmax(0,_1fr))] divide-x text-sm bg-white border-x border-b !border-gray-400 divide-gray-400 items-center  text-center font-semibold leading-10">
+        <div>&#8203;</div>
+        <div class="col-span-3">&#8203;</div>
+        <div class="col-span-2">&#8203;</div>
+        <div class="col-span-4">&#8203;</div>
+        <div class="col-span-2">&#8203;</div>
+        <div class="text-left pl-2 col-span-12">&#8203;</div>
+        <div class="col-span-2">&#8203;</div>
+        <div class="col-span-2 grid items-center">&#8203;</div>
       </div>
-    </div>
+    </template>
+    <div class="flex items-center justify-between mt-3">
+      <div class="flex items-center gap-4">
+        <p>Hiển thị</p>
+        <select
+          v-model="itemPerPage"
+          class="border !border-gray-400 rounded-sm px-2 py-1 !w-20 focus:outline-none focus:!border-gray-400 focus:ring-0">
+          <option :value="5">5</option>
+          <option :value="10">10</option>
+          <option :value="20">20</option>
+        </select>
+      </div>
+      <div class="flex items-center gap-2">
+        <p>Trang:</p>
+        <div class="flex items-center gap-3">
+          <ul class="flex items-center gap-1"> 
+            <li v-for="page in pageNumbers" :key="page" @click="onChangePage(page)">
+                <span aria-current="page" class="page-number" :class="{ 'active': page === currentPage }">
+                  {{ page }}
+                </span>
+            </li>
+        </ul>
+        </div>
+      </div>
+    </template>
   </div>
 </template>
 
