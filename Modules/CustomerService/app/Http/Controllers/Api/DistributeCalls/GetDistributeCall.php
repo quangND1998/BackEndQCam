54957e4e594554988e37d9dab79021806bad6897
dqhuy78 <dqhuy78@gmail.com>
2024-01-29 23:56:15 +0700

    Take 20

--- Modules/CustomerService/app/Http/Controllers/Api/DistributeCalls/GetDistributeCall.php
+++ Modules/CustomerService/app/Http/Controllers/Api/DistributeCalls/GetDistributeCall.php
@@ -12,17 +12,18 @@ class GetDistributeCall extends Controller
 {
     public function __invoke(Request $request)
     {
-        $fromDate = Carbon::createFromFormat('Y-m-d', $request->from_date)->format('Y-m-d');
-        $toDate = Carbon::createFromFormat('Y-m-d', $request->to_date)->format('Y-m-d');
+        $fromDate = Carbon::createFromFormat('Y-m-d', $request->from_date)->subDay(1)->format('Y-m-d');
+        $toDate = Carbon::createFromFormat('Y-m-d', $request->to_date)->addDay(1)->format('Y-m-d');
         $distributeCalls = DistributeCall::where('cskh_id', auth()->id())
-            ->with(['orderPackage.customer', 'orderPackage.product_service'])
+            ->with(['orderPackage.customer', 'orderPackage.product_service', 'orderPackage.product_service_owner'])
             ->whereBetween('date_call', [$fromDate, $toDate])
             ->get();
+
         $orderPackagePlans = [];
         $distributeCalls->each(function ($distributeCall) use (&$orderPackagePlans) {
             if (isset($orderPackagePlans[$distributeCall->order_package_id])) {
                 $dayOfWeek = Carbon::parse($distributeCall->date_call)->dayOfWeek ? Carbon::parse($distributeCall->date_call)->dayOfWeek : 7;
-                $orderPackagePlans[$distributeCall->order_package_id]['plans'][$dayOfWeek] = $distributeCall->state;
+                $orderPackagePlans[$distributeCall->order_package_id]['plans']['day_' . $dayOfWeek]['call'] = $distributeCall->state;
             } else {
                 $dayOfWeek = Carbon::parse($distributeCall->date_call)->dayOfWeek? Carbon::parse($distributeCall->date_call)->dayOfWeek : 7;
                 $orderPackagePlans[$distributeCall->order_package_id] = [
@@ -32,21 +33,25 @@ class GetDistributeCall extends Controller
                     'activeDate' => $distributeCall->orderPackage->time_approve,
                     'customerId' => $distributeCall->orderPackage->customer->id,
                     'plans' => [
-                        $dayOfWeek => $distributeCall->state,
+                        'day_' . $dayOfWeek => [
+                            'call' => $distributeCall->state,
+                            'order' => false
+                        ]
                     ],
                 ];
             }
         });
-        Order::where('product_service_owner_id', $distributeCalls->pluck('orderPackage.product_service.id')->toArray())
+
+        Order::where('sale_id', auth()->id())
             ->with(['product_service.order_package', 'customer', 'product_service.product'])
             ->where('type', 'gift_delivery')
             ->whereIn('status', ['pending', 'packing', 'shipping', 'completed'])
             ->whereBetween('delivery_appointment', [$fromDate, $toDate])
             ->get()
             ->each(function ($order) use (&$orderPackagePlans) {
-                $dayOfWeek = Carbon::parse($order->delivery_appoitment)->dayOfWeek ? Carbon::parse($order->delivery_appoitment)->dayOfWeek : 7;
+                $dayOfWeek = Carbon::parse($order->delivery_appointment)->dayOfWeek ? Carbon::parse($order->delivery_appointment)->dayOfWeek : 7;
                 if (isset($orderPackagePlans[$order->product_service->order_id])) {
-                    $packageOrders[$order->product_service->order_id]['plans'][$dayOfWeek]['order'] = true;
+                    $orderPackagePlans[$order->product_service->order_id]['plans']['day_' . $dayOfWeek]['order'] = true;
                 } else {
                     $orderPackagePlans[$order->product_service->order_id] = [
                         'orderPackageId' => $order->product_service->order_package->idPackage,
@@ -55,14 +60,14 @@ class GetDistributeCall extends Controller
                         'activeDate' => $order->product_service->order_package->time_approve,
                         'customerId' => $order->customer->id,
                         'plans' => [
-                            $dayOfWeek => [
-                                'plan' => null,
+                            'day_' . $dayOfWeek => [
+                                'call' => null,
                                 'order' => true
+                            ]
                         ],
                     ];
                 }
             });
-
 
         return response()->json([
             'message' => 'Ok',
--- resources/js/Components/CustomerService/CycleTime.vue
+++ resources/js/Components/CustomerService/CycleTime.vue
@@ -23,6 +23,10 @@ const props = defineProps({
   allowPopover: {
     required: false,
     default: false,
+  },
+  multipler: {
+    required: false,
+    default: 0,
   }
 });
 
@@ -44,18 +48,20 @@ const date = computed(() => {
     return moment(props.data.date_time, 'YYYY-MM-DD HH:mm:ss');
   }
 
-  const nextDate = moment(props.startDate, 'YYYY-MM-DD HH:mm:ss').add(((props.position + 1) * CYCLE_TIME), 'days');
-  return nextDate.weekday() !== 7 ? nextDate : nextDate.subtract(1, 'day');
+  const additionalDate = props.multipler * 12 * CYCLE_TIME
+  const nextDate = moment(props.startDate, 'YYYY-MM-DD HH:mm:ss').add(((props.position + 1) * CYCLE_TIME + additionalDate), 'days');
+
+  return nextDate.weekday() === 0 ? nextDate.subtract(1, 'day') : nextDate;
 });
 const cellStyle = computed(() => {
-  if (props.data && props.data?.status === 'complete') return 'bg-emerald-600 text-white';
-  if (!props.data && props.showEmpty) return 'bg-zinc-700 text-zinc-700 select-none';
-  if (props.data && props.data?.state === SCHEDULE_VISIT_STATE.COMPLETE) return 'bg-emerald-600 text-white';
-  if (props.data && props.data?.state === SCHEDULE_VISIT_STATE.CANCEL) return 'bg-red-600 text-white';
-  if (props.data && props.data?.state === SCHEDULE_VISIT_STATE.PENDING && date.value.diff(new Date(), 'days') < 0) return 'bg-red-600 text-white';
-  if (props.data && date.value.diff(new Date(), 'days') >= 0) return 'bg-yellow-600 text-white';
-  if (date.value.diff(new Date(), 'days') < 0 && !props.allowEmpty) return 'bg-red-600 text-white';
-  if (!props.data) return 'bg-zinc-700 text-white';
+  if (props.data && props.data?.status === 'complete') return 'bg-[#4F8D06] text-white';
+  if (!props.data && props.showEmpty) return 'bg-[#3D3C3C] text-[#3D3C3C] select-none';
+  if (props.data && props.data?.state === SCHEDULE_VISIT_STATE.COMPLETE) return 'bg-[#4F8D06] text-white';
+  if (props.data && props.data?.state === SCHEDULE_VISIT_STATE.CANCEL) return 'bg-[#FF0000] text-white';
+  if (props.data && props.data?.state === SCHEDULE_VISIT_STATE.PENDING && date.value.diff(new Date(), 'days') < 0) return 'bg-[#FF0000] text-white';
+  if (props.data && date.value.diff(new Date(), 'days') >= 0) return 'bg-[#CAAA00] text-white';
+  if (date.value.diff(new Date(), 'days') < 0 && !props.allowEmpty) return 'bg-[#FF0000] text-white';
+  if (!props.data) return 'bg-[#3D3C3C] text-white';
 });
 const displayText = computed(() => {
   if (!props.data && props.showEmpty) return 'Chưa thăm';
@@ -87,7 +93,7 @@ const isFirstOrderDelivery = computed(() => {
         : displayText  }}
     </p>
     <div v-if="data && data.order_number && allowPopover" v-show="showPopover" ref="floating" :style="floatingStyles"
-      class="bg-white rounded-lg border !border-gray-400 py-3 z-50">
+      class="bg-white rounded-lg border !border-[#AEAEAE] py-3 z-50">
       <div ref="floatingArrow" class="triangle" :style="{
         position: 'absolute',
         left:
@@ -100,20 +106,20 @@ const isFirstOrderDelivery = computed(() => {
       <div class="mb-2 px-3 flex items-center justify-between">
         <p class="text-left l-3 font-semibold">Lịch sử nhận quà lần {{ position + 1 }}</p>
         <button
-          class="rounded-md bg-orange-600 text-white relative font-medium px-3 py-2"
+          class="rounded-md bg-[#FF6100] text-white relative font-medium px-3 py-2"
           @click="onUpdateOrder"
         >
           Cập nhật đơn
         </button>
       </div>
-      <div class="grid grid-cols-12 items-center bg-gray-400 text-white font-bold leading-6 w-[700px]">
+      <div class="grid grid-cols-12 items-center bg-[#AEAEAE] text-white font-bold leading-6 w-[700px]">
         <div>STT</div>
         <div class="col-span-3">Ngày</div>
         <div class="col-span-8 text-left pl-2">Mô tả</div>
       </div>
       <div class="h-[300px] overflow-y-auto">
         <div v-for="(history, index) in data?.shipping_history || []" :key="history.id"
-          class="grid grid-cols-12  divide-x divide-gray-400 border-gray-400 border-b border-x text-sm">
+          class="grid grid-cols-12  divide-x divide-[#AEAEAE] border-[#AEAEAE] border-b border-x text-sm">
           <div>{{ index + 1 }}</div>
           <div class="col-span-3">{{ moment(history.created_at, 'YYYY-MM-DD HH:mm:ss').format('DD/MM/YYYY') }}</div>
           <div class="col-span-8 text-left pl-2">{{ history.note }}</div>
