a45f8587e2e67d28bf4ee3175a366f2e2f85ff57
HuyDQ <dqhuy78@gmail.com>
2024-02-01 19:02:02 +0700

    Update CreateComplaint.php

--- Modules/CustomerService/app/Http/Controllers/Api/Complaints/CreateComplaint.php
+++ Modules/CustomerService/app/Http/Controllers/Api/Complaints/CreateComplaint.php
@@ -14,13 +14,19 @@ class CreateComplaint extends Controller
             'description' => 'required|string',
             'severity' => 'required|in:normal,urgent,critical',
             'role_id' => 'required|exists:roles,id',
+            'package_id' => 'required|exists:order_packages,idPackage',
         ]);
 
+        $orderPackage = OrderPackage::where('idPackage', $request->package_id)->with('product_service_owner')->firstOrFaild();
+        $productServiceOwnerId = $orderPackage->product_service_owner->id;
         $request->merge([
             'user_id' => $request->customerId,
             'type' => 'cskh',
             'state' => 0,
+            'counselor_staff_id' => auth()->id(),
+            'product_service_owner_id' => $productServiceOwnerId,
         ]);
+        
         $complaint = ComplaintManagement::create(
             $request->only([
                 'user_id',
@@ -29,6 +35,8 @@ class CreateComplaint extends Controller
                 'role_id',
                 'type',
                 'state',
+                'counselor_staff_id',
+                'product_service_owner_id',
             ])
         );
 
