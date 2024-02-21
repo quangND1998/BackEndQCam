31cf4bbb0174b73ff61398779e3b889b545c7881
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2024-02-21 17:25:23 +0700

    quangnd fix updateuser api

--- resources/js/Components/UploadImage.vue
+++ resources/js/Components/UploadImage.vue
@@ -175,11 +175,18 @@ const Delete = (img) => {
                         class="cursor-pointer w-16 h-16 border-dashed items-center border-gray-500 mx-1 justify-center flex border rounded-lg">
                         <BaseIcon :path="mdiPlus" class="" :size="16" />
                     </label>
+
+                    <label :for="id" v-else-if="max_files == null"
+                        class="cursor-pointer w-16 h-16 border-dashed items-center border-gray-500 mx-1 justify-center flex border rounded-lg">
+                        <BaseIcon :path="mdiPlus" class="" :size="16" />
+                    </label>
                     <input @change="onFileChange"
                         v-if="((old_images ? old_images.length : 0) + form.images.length) < max_files"
                         class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 hidden"
                         :id="id" type="file" :multiple="multiple" accept="image/*">
-
+                    <input @change="onFileChange" v-else-if="max_files == null"
+                        class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 hidden"
+                        :id="id" type="file" :multiple="multiple" accept="image/*">
                 </div>
 
             </div>
