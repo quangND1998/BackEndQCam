9f0dd2c103337493c784acafbeedfa46472e70b8
Nguyen Nga <ngant1096@gmail.com>
2024-03-01 13:29:41 +0700

    edit

--- Modules/Tree/database/migrations/2024_01_18_091454_addNote_history_cares_table.php
+++ Modules/Tree/database/migrations/2024_01_18_091454_addNote_history_cares_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('history_cares', function (Blueprint $table) {
-            $table->longText('note')->nullable();
-        });
+        // Schema::table('history_cares', function (Blueprint $table) {
+        //     $table->longText('note')->nullable();
+        // });
     }
 
     /**
@@ -21,8 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('history_cares', function (Blueprint $table) {
-            $table->dropColumn('note');
-        });
+        // Schema::table('history_cares', function (Blueprint $table) {
+        //     $table->dropColumn('note');
+        // });
     }
 };
