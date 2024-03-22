a5c76f40f320d831edbef58ce19256a13024f0df
Nguyen Nga <ngant1096@gmail.com>
2024-03-22 16:27:33 +0700

    edit

---
+++ app/Http/Middleware/LogRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Log;
+
+class LogRequest
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        logger($request);
+        return $next($request);
+    }
+}
